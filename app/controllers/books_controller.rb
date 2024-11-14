780d3e80cd708c5b5b63605250786ece7f373951
jminterwebs <jmverdone@gmaill.com>
2024-11-13 21:37:03 -0500

    Books controller stubbed out

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,33 @@
 class BooksController < ApplicationController
+  has_many :reviews
+
+  before_action :authenticate_user!, except: [:index, :show]
+
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
