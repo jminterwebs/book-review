8f6664e1b82b4b950e8d50c905e78757344c2022
jminterwebs <jmverdone@gmaill.com>
2024-11-14 10:39:16 -0500

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   resources :books do
     resources :reviews
   end
-  root 'application#index'
+  root 'books#index'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:show] do
+    resources :reviews, only: [:index, :edit, :update, :destroy] # You can add other actions as needed
+  end
 end
