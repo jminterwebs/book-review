07724eea88011d9f426f1f2950936950904b4bdf
jminterwebs <jmverdone@gmaill.com>
2024-11-14 10:39:55 -0500

    rescope review uniquness

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,6 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :book
 
-  validates :user_id, uniqueness: { scope: :book_id, message: "You can only submit one review per book." }
+  validates :user_id, uniqueness: { scope: :book_id, message: "You can only submit one review per book." }, unless: :persisted?
 
 end
