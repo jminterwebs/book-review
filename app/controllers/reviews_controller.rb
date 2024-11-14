11a8e3ba6d880da7c8a488196749ddfed540d187
jminterwebs <jmverdone@gmaill.com>
2024-11-14 09:35:56 -0500

    Added turbo to dynamicly update reviews if a user wants to add a review to a book.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,27 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @book = Book.find(params[:book_id])
+    @review = @book.reviews.build(review_params)
+  end
+
+  def create
+    @book = Book.find(params[:book_id])
+    @review = @book.reviews.build(review_params)
+
+    if @review.save
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to @book, notice: "Review was successfully added." }
+      end
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def review_params
+    params.require(:review).permit(:content, :rating, :user_id)
+  end
 end
