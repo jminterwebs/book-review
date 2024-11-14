314cc682b4c1ec81db76c63e0dae0ecafddb56ef
jminterwebs <jmverdone@gmaill.com>
2024-11-14 08:06:07 -0500

    add users to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_11_14_023509) do
+ActiveRecord::Schema.define(version: 2024_11_14_130213) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -27,7 +27,9 @@ ActiveRecord::Schema.define(version: 2024_11_14_023509) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "book_id", null: false
+    t.integer "user_id", null: false
     t.index ["book_id"], name: "index_reviews_on_book_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,9 +40,11 @@ ActiveRecord::Schema.define(version: 2024_11_14_023509) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "reviews", "books"
+  add_foreign_key "reviews", "users"
 end
