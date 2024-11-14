d8e327481d29ffdee7da05da877a2d8a5698a19e
jminterwebs <jmverdone@gmaill.com>
2024-11-14 10:39:41 -0500

    edit and destroy review methods

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,14 @@
 class ReviewsController < ApplicationController
-
+  before_action :authenticate_user!
+  before_action :set_user, only: [:index, :edit, :update, :destroy]
   def new
     @book = Book.find(params[:book_id])
     @review = @book.reviews.build(review_params)
   end
 
+  def index
+    @reviews = @user.reviews
+  end
   def create
     @book = Book.find(params[:book_id])
     @review = @book.reviews.build(review_params)
@@ -19,8 +23,34 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def edit
+    @review = Review.find(params[:id])
+    @user = current_user
+  end
+  def update
+    binding.pry
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to user_reviews_path(@user), notice: 'Review was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+
+    redirect_to user_reviews_path(@user), notice: 'Review was successfully deleted.'
+
+  end
 
   private
+
+  def set_user
+    @user = current_user
+  end
   def review_params
     params.require(:review).permit(:content, :rating, :user_id)
   end
